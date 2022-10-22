90b22d2b088d53320e087020dd3f2dd1395b96c6
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-10-23 09:42:57 +1100

    setup of Router and Styled Components

---
+++ src/components/home.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+	background-color: yellow;
+	width: 100vw;
+	height: 100vh;
+`;
+const NavBar = styled.div``;
+const LeftPane = styled.div``;
+const CenterPane = styled.div``;
+
+const RightPane = styled.div``;
+
+const Home = () => {
+	return (
+		<Container>
+      Hello World
+		</Container>
+	); 
+};
+
+export default Home;
